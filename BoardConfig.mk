7f37b8701aae73c50048c2b709350852be4e2f28
Adarsh-MR <adarshmr1998@gmail.com>
2017-05-07 10:48:14 +0000

    msm8852_64: gaurd some stuff
    Signed-off-by: Adarsh-MR <adarshmr1998@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,7 @@ TARGET_2ND_CPU_VARIANT := cortex-a53
 TARGET_CPU_CORTEX_A53 := true
 
 TARGET_NO_BOOTLOADER := false
-TARGET_NO_KERNEL := false
+#TARGET_NO_KERNEL := false
 BOOTLOADER_GCC_VERSION := arm-eabi-4.8
 BOOTLOADER_PLATFORM := msm8952 # use msm8952 LK configuration
 #MALLOC_IMPL := dlmalloc
@@ -54,6 +54,7 @@ TARGET_USE_MDTP := true
 
 BOARD_SECCOMP_POLICY := device/qcom/$(TARGET_BOARD_PLATFORM)$(TARGET_BOARD_SUFFIX)/seccomp
 
+ifneq ($(filter hydrogen kenzo,$(TARGET_DEVICE)),)
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x04000000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x04000000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2684354560
@@ -63,6 +64,7 @@ BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
 BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+endif
 
 # Enable suspend during charger mode
 BOARD_CHARGER_ENABLE_SUSPEND := true
@@ -77,7 +79,7 @@ TARGET_USES_QCOM_BSP := true
 TARGET_NO_RPC := true
 
 #TARGET_TS_MAKEUP := true
-
+ifneq ($(filter hydrogen kenzo,$(TARGET_DEVICE)),)
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
 #BOARD_KERNEL_SEPARATED_DT := true
 
@@ -91,6 +93,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_USES_UNCOMPRESSED_KERNEL := false
+endif
 
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
@@ -105,9 +108,9 @@ MAX_EGL_CACHE_SIZE := 2048*1024
 BOARD_EGL_CFG := device/qcom/msm8952_64/egl.cfg
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 # Add NON-HLOS files for ota upgrade
-ADD_RADIO_FILES := true
-TARGET_RECOVERY_UPDATER_LIBS += librecovery_updater_msm
-TARGET_INIT_VENDOR_LIB := libinit_msm
+#ADD_RADIO_FILES := true
+#TARGET_RECOVERY_UPDATER_LIBS += librecovery_updater_msm
+#TARGET_INIT_VENDOR_LIB := libinit_msm
 
 #add suffix variable to uniquely identify the board
 TARGET_BOARD_SUFFIX := _64
@@ -125,18 +128,6 @@ TARGET_CRYPTFS_HW_PATH := device/qcom/common/cryptfs_hw
 TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
 
 MAX_VIRTUAL_DISPLAY_DIMENSION := 2048
-
-# Enable dex pre-opt to speed up initial boot
-ifeq ($(HOST_OS),linux)
-  ifeq ($(WITH_DEXPREOPT),)
-    WITH_DEXPREOPT := true
-    WITH_DEXPREOPT_PIC := true
-    ifneq ($(TARGET_BUILD_VARIANT),user)
-      # Retain classes.dex in APK's for non-user builds
-      DEX_PREOPT_DEFAULT := nostripping
-    endif
-  endif
-endif
 
 FEATURE_QCRIL_UIM_SAP_SERVER_MODE := true
 
